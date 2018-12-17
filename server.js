e92756643369e781befda4baf0a3e9b19ec4670f
Jelle Overbeek <j.overbeek@recognize.nl>
2018-12-17 23:10:24 +0100

    first tests with abstract SDK

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const nunjucks = require('nunjucks');
+
+const Abstract = require('abstract-sdk');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -15,8 +19,45 @@ nunjucks.configure('src/views', {
 
 app.use(express.static(__dirname + '/src/assets'));
 
+// Create a client
+const abstract = Abstract.Client();
+
+const theProjects = [];
+
+async function run() {
+    // Query all projects
+    const projects = await abstract.projects.list();
+
+    // Iterate through each project
+    for (const project of projects) {
+
+        let projectObj = {
+            info: project,
+            comments: []
+        }
+
+        const comments = await abstract.comments.list({
+            projectId: project.id
+        });
+
+        for (const comment of comments) {
+            console.log(comment)
+            projectObj.comments.push(comment)
+        }
+
+        theProjects.push(projectObj)
+    }
+    //     console.log(comment);
+    // }
+
+}
+
+run();
+
 app.get('/', function (req, res) {
-    res.render('index.html');
+    res.render('index.html', {
+        data: theProjects
+    });
 });
 
 http.listen(port, function () {

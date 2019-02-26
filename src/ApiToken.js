0a33aed1c1abe798b957541f34ce410ba0544cdb
Jelle Overbeek <overbeekjelle@gmail.com>
2019-02-26 23:16:03 +0100

    some progress on adding the API Token

--- src/ApiToken.js
+++ src/ApiToken.js
@@ -3,6 +3,31 @@ import PrimaryButton from "./PrimaryButton";
 import "./ApiToken.css";
 
 class ApiTokenForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: ""
+    };
+  }
+
+  storeApiToken(event) {
+    event.preventDefault();
+
+    const token = event.target.querySelector(".api-token").value;
+    localStorage.setItem("ABSTRACT_TOKEN", token);
+  }
+
+  getApiToken() {
+    const token = localStorage.getItem("ABSTRACT_TOKEN");
+    return token;
+  }
+
+  componentDidMount() {
+    this.setState({
+      token: this.getApiToken()
+    });
+  }
+
   render() {
     return (
       <div className="container--small">
@@ -14,8 +39,8 @@ class ApiTokenForm extends React.Component {
           </a>{" "}
           section in your Personal Settings of Abstract. The token will be stored locally. div.
         </p>
-        <form className="form--api-token">
-          <input className="input--big" type="text" placeholder="Paste token here" />
+        <form className="form--api-token" onSubmit={this.storeApiToken}>
+          <input className="input--big api-token" type="text" placeholder={this.state.token} />
           <PrimaryButton text="Save" />
         </form>
       </div>

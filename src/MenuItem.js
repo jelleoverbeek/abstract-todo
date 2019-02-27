28fb154ee65d353ecbcfee07fb2efb591abdb494
Jelle Overbeek <overbeekjelle@gmail.com>
2019-02-27 20:45:01 +0100

    added active state

--- src/MenuItem.js
+++ src/MenuItem.js
@@ -1,19 +1,16 @@
 import React from "react";
-import "./Button.css";
 import "./MenuItem.css";
 import Avatar from "./Avatar";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 class MenuItem extends React.Component {
   render() {
     return (
       <li>
-        <Link to={`/comments/${this.props.projectId}/${this.props.branchId}`}>
-          <button className="MenuItem">
-            <span>{this.props.name}</span>
-            <Avatar userId={this.props.userId} />
-          </button>
-        </Link>
+        <NavLink to={`/comments/${this.props.projectId}/${this.props.branchId}`} className="MenuItem" activeClassName="MenuItem--active">
+          <span>{this.props.name}</span>
+          <Avatar userId={this.props.userId} />
+        </NavLink>
       </li>
     );
   }

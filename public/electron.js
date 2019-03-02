33002617b3fc6627801c1f2ed1842fe5d2347218
Jelle Overbeek <overbeekjelle@gmail.com>
2019-03-02 13:00:55 +0100

    added code to build app

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,4 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
+const { app, Menu, BrowserWindow } = require("electron");
 const path = require("path");
 const url = require("url");
 const isDev = require("electron-is-dev");
@@ -18,6 +15,14 @@ function createWindow() {
       nodeIntegration: true
     }
   });
+
+  if (isDev) {
+    console.log("Running in development");
+    mainWindow.webContents.openDevTools();
+  } else {
+    console.log("Running in production");
+  }
+
   mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
   mainWindow.on("closed", () => (mainWindow = null));
 }
@@ -35,3 +40,81 @@ app.on("activate", () => {
     createWindow();
   }
 });
+
+const template = [
+  {
+    label: "Edit",
+    submenu: [
+      { role: "undo" },
+      { role: "redo" },
+      { type: "separator" },
+      { role: "cut" },
+      { role: "copy" },
+      { role: "paste" },
+      { role: "pasteandmatchstyle" },
+      { role: "delete" },
+      { role: "selectall" }
+    ]
+  },
+  {
+    label: "View",
+    submenu: [
+      { role: "reload" },
+      { role: "forcereload" },
+      { role: "toggledevtools" },
+      { type: "separator" },
+      { role: "resetzoom" },
+      { role: "zoomin" },
+      { role: "zoomout" },
+      { type: "separator" },
+      { role: "togglefullscreen" }
+    ]
+  },
+  {
+    role: "window",
+    submenu: [{ role: "minimize" }, { role: "close" }]
+  },
+  {
+    role: "help",
+    submenu: [
+      {
+        label: "Learn More",
+        click() {
+          require("electron").shell.openExternal("https://electronjs.org");
+        }
+      }
+    ]
+  }
+];
+
+if (process.platform === "darwin") {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      { role: "about" },
+      { type: "separator" },
+      { role: "services" },
+      { type: "separator" },
+      { role: "hide" },
+      { role: "hideothers" },
+      { role: "unhide" },
+      { type: "separator" },
+      { role: "quit" }
+    ]
+  });
+
+  // Edit menu
+  template[1].submenu.push(
+    { type: "separator" },
+    {
+      label: "Speech",
+      submenu: [{ role: "startspeaking" }, { role: "stopspeaking" }]
+    }
+  );
+
+  // Window menu
+  template[3].submenu = [{ role: "close" }, { role: "minimize" }, { role: "zoom" }, { type: "separator" }, { role: "front" }];
+}
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
